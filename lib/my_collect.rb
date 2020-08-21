c838d6d24de9ff94bfffc55bfc4d50f580578217
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-21 16:45:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,29 @@
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+
+
+
+
+
+
+
+
+
+#def hello(array)
+  #i = 0
+  #collection = []
+  #while i < array.length
+    #collection << yield(array[i])
+    #i += 1
+  #end
+  #collection
+#end
